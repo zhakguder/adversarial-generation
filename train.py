3d3e70fbe3e823934334898c3cf4b9666807f682
Zeynep Hakguder <zeynep.hakguder@huskers.unl.edu>
2019-05-27 14:14:07 -0500

    rename file

--- train.py
+++ train.py
@@ -1,55 +1,149 @@
-import tensorflow as tf
-from tensorflow import keras
+import warnings
+warnings.filterwarnings("ignore")
+from data_2 import *
+from estimators_2 import Generator, AdversarialGenerator, NetworkGenerator
+from settings import get_settings
+from data_generators import combined_data_generators
+from applications import MnistEval, AdversarialEval, Cifar10Eval
+from utils import timeit_context
+from sys import exit
 from ipdb import set_trace
-from models import build_net, VAE
+from classifier import Classifier
+import numpy as np
 
-def loss(inputs, reconstruction, mean, logvar):
+from empirical import patch_fitness_grad
 
-    BCE = tf.reduce_sum(tf.keras.metrics.binary_crossentropy(inputs, reconstruction))
-    KL = tf.reduce_sum(-0.5 * (1 + logvar - mean**2 - tf.exp(logvar)))
-    return BCE + KL
-
-
-def grad(inputs, targets):
-    relu_dims = [40,20]
-    softmax_dim = 10
-    latent_dim = 10
-    batch_size = 64
-    with tf.GradientTape() as tape:
-        reconstruction = VAE(inputs, relu_dims, latent_dim, batch_size)
-        loss_value = loss(inputs, reconstruction, mean, logvar)
-    return tape.gradient(loss_value, model.trainable_variables)
-
-if __name__ == '__main__':
-
-    from data import x_train, y_train, x_test, y_test
-
-    relu_dims = [40,20]
-    softmax_dim = 10
-    latent_dim = 10
-    batch_size = 64
+EPS = tf.keras.backend.epsilon()
 
 
-    inputs = keras.Input(shape=(784,), name = 'mnist_img')
-    #mean, logvar, reconstruction = VAE(inputs, relu_dims, latent_dim, batch_size)
-    reconstruction = VAE(inputs, relu_dims, latent_dim, batch_size)
-    model = keras.Model(inputs=inputs, outputs=reconstruction, name='crazy_vae')
-    #optimizer = tf.keras.optimizers.RMSprop()
-    #model.summary()
-    #keras.utils.plot_model(model, 'my_first_model.png')
+flags, params = get_settings()
+AUTOENCODE = flags['autoencode']
+APP = flags['app']
+CLASSIFIER = flags['classifier_train']
+LOAD_CLASSIFIER = flags['load_classifier']
+TRAIN_ADVERSARIAL = flags['train_adversarial']
+ONLY_CLASSIFIER = flags['only_classifier']
+LEARNING_RATE = params['learning_rate']
 
-    for i in range(4):
-        grads = grad(inputs, inputs)
-        optimizer.apply_gradients(zip(grads, model.trainable_variables), global_step=tf.train.get_or_create_global_step())
-        print("Loss at step {:03d}: {:.3f}".format(i, loss(model, training_inputs, training_outputs)))
-        print("Final loss: {:.3f}".format(loss(model, training_inputs, training_outputs)))
-        print("W = {}, B = {}".format(model.W.numpy(), model.B.numpy()))
+EPOCHS = flags['epochs']
+N_DATA = 60000
+
+optimizer = tf.keras.optimizers.Adam(learning_rate=LEARNING_RATE)
+loss_history = []
+
+if ONLY_CLASSIFIER and CLASSIFIER: # train classifier for adversarial generation
+    classifier = Classifier(training=CLASSIFIER)
+    if LOAD_CLASSIFIER:
+        classifier.load()
+    for epoch in range(EPOCHS):
+        print('Starting epoch: {}'.format(epoch))
+        for step, data in enumerate(classifier.get_input_data()):
+            x_train, y_train = data
+            if step > int(np.ceil(N_CIFAR10/x_train.shape[0])):
+                break
+            with tf.GradientTape() as tape:
+                logits = classifier(x_train)
+                loss_value = tf.nn.softmax_cross_entropy_with_logits(y_train, logits)
+                if step % 20 == 0:
+                    print('Step: {}'.format(step))
+                    print('{} classifier loss: {}'.format(flags['dataset'], tf.reduce_mean(loss_value)))
+                grads = tape.gradient(loss_value, classifier.trainable_variables)
+                optimizer.apply_gradients(zip(grads, classifier.trainable_variables))
+                for stept, test_data in enumerate(classifier.get_input_data(train=False)):
+                    if stept ==  1:
+                        break
+                    x_test, y_test = test_data
+                    _, predicted_classes = classifier.classify(x_test)
+                    if step % 20 == 0:
+                        classifier.accuracy(tf.argmax(y_test, axis=1), predicted_classes)
+    classifier.save()
+    exit()
+
+elif ONLY_CLASSIFIER and LOAD_CLASSIFIER and not CLASSIFIER:
+    classifier = Classifier(training=CLASSIFIER)
+    classifier.load()
+    for stept, test_data in enumerate(classifier.get_input_data(train=False)):
+        if stept ==  1:
+            break
+    x_test, y_test = test_data
+    _, predicted_classes = classifier.classify(x_test)
+    classifier.accuracy(tf.argmax(y_test, axis=1), predicted_classes)
+    exit()
+
+elif AUTOENCODE:
+    if APP == 'adversarial':
+        # labels and logits predicted by a classifier
+        model = AdversarialGenerator()
+        evaluator = AdversarialEval()
+    else:
+        raise NotImplementedError ('No use case for autoencoding without adversarial generation.')
+else:
+    model = NetworkGenerator()
+    model.get_input_data()
+    evaluator = MnistEval(model.aux_data) if flags['dataset'] == 'mnist' else Cifar10Eval(model.aux_data)
+
+if flags['load_generator']:
+    model.load()
+
+for epoch in range(EPOCHS):
+    print('Start of epoch %d' % (epoch,))
+    # Iterate over the batches of the dataset.
+    for step, data in enumerate(model.get_input_data()):
+
+        if type(data)==tuple:
+            try:
+                x_train, y_train, y_logits  = data
+            except:
+                x_train, y_train = data
+        else:
+            x_train = data
+
+        if step > int(np.ceil(N_DATA/x_train.shape[0])):
+            break
+
+        print('Step: {}'.format(step))
+
+        with tf.GradientTape(persistent=True) as tape:
+            if APP =='generated':
+                output = model(x_train)
+                means = model.get_cluster_centers()
+                with tf.GradientTape() as eval_tape:
+                    evaluator.eval_clusters(means)
+                evaluator.summary()
+
+            elif APP == 'adversarial':
+                # TODO
+                # SINGLE IMAGE OR WHAT????
+                # TODO:
+                # 1. evaluate for each possible class when in adversarial training
+                #else:
+
+                qs = model((x_train, y_train))
+                correct = tf.reshape(x_train, (-1, model.output_dim))
+                if not TRAIN_ADVERSARIAL:
+                    evaluator.set_latent_dists(model.latent_prior, model.approx_posterior)
+                with tf.GradientTape() as eval_tape:
+                    evaluator.eval_clusters(correct, model.cluster_dictionary, y_train)
+                evaluator.summary()
+
+            f_gradients = eval_tape.gradient(evaluator.metrics['f'], evaluator.net.trainable_variables)
+            f_gradient = tf.concat([tf.reshape(v, (-1, )) for v in f_gradients], axis=0)
+            f_gradient = tf.tile(tf.reshape(f_gradient, (1,-1)), [flags['latent_batch_size'],1])
+
+            output1 = patch_fitness_grad(output, f_gradient)
+
+            qs_1 = model.cluster_computations(output1)
+            #qs_0 = model.cluster_computations(output)
 
 
+            #loss_value_0 = tf.math.log(qs_0) # only qs
+            loss_value_1 = tf.math.log(qs_1+EPS) # q_s + fitness
+            model.loss.append(loss_value_1.numpy().mean())
+            model.summary()
+        set_trace()
+        #grads_0 = tape.gradient(loss_value_0, model.trainable_variables)
+        grads_1 = tape.gradient(loss_value_1, model.trainable_variables)
+        optimizer.apply_gradients(zip(grads_1, model.trainable_variables))
 
-    #history = model.fit(x_train, [x_train], batch_size=batch_size, epochs=5, validation_split=0.2)
-
-    #test_scores = model.evaluate(x_test, y_test, verbose=0)
-
-    #print('Test loss:', test_scores[0])
-    #print('Test accuracy:', test_scores[1])
+    if epoch % 100 == 0:
+        model.save()
